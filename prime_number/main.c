e41ffadce991408d1d8171fda6521657d59ff566
UmbaDumba <dkdi3104@gmail.com>
2025-02-20 09:43:17 +0900

    find prime number

---
+++ prime_number/main.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	int n;
+
+	printf("2 이상의 정수를 입력하세요 >> ");
+	scanf("%d", &n);
+
+	int primes[100];
+	int count = 0;
+	for (int i = 2; i <= n; i++) {
+		//int flag = 1;
+		int j;
+		for (j = 0; j < count; j++) {
+			if (i % primes[j] == 0) {
+				//flag = 0;
+				break;
+			}
+		}
+		if (j == count) {
+			// 소수임!
+			printf("%3d ", i);
+			primes[count] = i;
+			count++;
+			if (count % 5 == 0) printf("\n");
+			
+		}
+
+
+	}
+
+	return 0;
+}
